fd1161278c18f20b1e32fe15be607780337ef3d6
Markus Voit <m-voit@users.noreply.github.com>
2019-10-24 17:51:27 +0200

    Rename exercise 3-1 files.

--- exercise-3-1/polymorphism.go
+++ exercise-3-1/polymorphism.go
@@ -1,44 +1,53 @@
-package main
+package polymorphism
 
 import "fmt"
 
+// Position :
 type Position struct {
 	x, y int
 }
 
+// GeoObject :
 type GeoObject struct {
 	position Position
 	color    int
 }
 
+// Circle :
 type Circle struct {
 	GeoObject
 	radius int
 }
 
+// Rectangle :
 type Rectangle struct {
 	GeoObject
 	width  int
 	height int
 }
 
+// Triangle :
 type Triangle struct {
 	GeoObject
 	a, b, c Position
 }
 
+// Painter :
 type Painter interface {
 	Paint()
 }
 
+// Paint :
 func (circle Circle) Paint() {
 	fmt.Printf("Painting circle with radius=%v at position=%v and color=%v\n", circle.radius, circle.position, circle.color)
 }
 
+// Paint :
 func (rectangle Rectangle) Paint() {
 	fmt.Printf("Painting rectangle with width=%v, height=%v at position=%v and color=%v\n", rectangle.width, rectangle.height, rectangle.position, rectangle.color)
 }
 
+// Paint :
 func (triangle Triangle) Paint() {
 	fmt.Printf("Painting triangle with a=%v, b=%v, c=%v at position=%v and color=%v\n", triangle.a, triangle.b, triangle.c, triangle.position, triangle.color)
 }
