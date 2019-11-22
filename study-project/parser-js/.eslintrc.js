0104e2c10d6ee90966de5bf26e2db68f5d0f5df9
Markus Voit <m-voit@outlook.com>
2019-11-22 15:10:10 +0100

    Use js file for eslint config.

---
+++ study-project/parser-js/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
