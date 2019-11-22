21b37057175ceb39c82eaf8423cc99f58c0c78ec
Markus Voit <m-voit@outlook.com>
2019-11-22 13:38:47 +0100

    Update docs.

--- study-project/parser-js/boolparser/boolparser.js
+++ study-project/parser-js/boolparser/boolparser.js
@@ -1,6 +1,5 @@
 /**
- * Parse the following grammar:
- * Expression := Or Spaces*
+ * Parse the following grammar: Expression := Or Spaces*
  * The syntax tree is exactly the one returned by Or.
  *
  * @param {any} input
@@ -11,8 +10,7 @@ function parseExpression(input) {
 }
 
 /**
- * Parse the following grammar:
- * Or := And ^ ("|" ^ Or)?
+ * Parse the following grammar: Or := And ^ ("|" ^ Or)?
  * If the parser for ("|" ^ Or)? produces nothing then parseOr will return the
  * tree returned by And. Otherwise parseOr will return a new Or Node containing
  * the sub-trees returned by the recursive calls. parseOr uses expect to parse
@@ -26,8 +24,7 @@ function parseOr(input) {
 }
 
 /**
- * Parse the following grammar:
- * And := Not ^ ("&" ^ And)?
+ * Parse the following grammar: And := Not ^ ("&" ^ And)?
  * If the parser for ("&" ^ And)? produces nothing then parseAnd will return the
  * tree returned by Not. Otherwise parseAnd will return a new And Node containing
  * the sub-trees returned by the recursive calls. parseAnd uses expect to parse
@@ -41,8 +38,7 @@ function parseAnd(input) {
 }
 
 /**
- * Parse the following grammar:
- * Not := "!"* ^ Atom
+ * Parse the following grammar: Not := "!"* ^ Atom
  * It delegates parsing "!"* to parseExclamationMarks and the construction of Not
  * nodes to makeNots. If there's no exclamation mark then parseNot will return
  * the tree parsed by parseAtom. Otherwise parseNot will wrap the atom in as many
@@ -56,9 +52,8 @@ function parseNot(input) {
 }
 
 /**
- * Parse the following grammar:
- * "!"*
- * It returns the number of exclamation marks in Result.Result as an int.
+ * Parse the following grammar: "!"*
+ * It returns the number of exclamation marks in Result. Result as an int.
  * parseExclamationMarks uses expect to parse the symbol "!", i. e. it actually
  * allows for Space* ^ "!".
  *
