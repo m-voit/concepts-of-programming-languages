81ebea1d5377bccad397f83a84a1ebb96b3d5928
Markus Voit <m-voit@outlook.com>
2019-12-19 18:32:55 +0100

    Partially implement js boolparser test.

--- study-project/parser-js/modules/boolparser/boolparser.test.js
+++ study-project/parser-js/modules/boolparser/boolparser.test.js
@@ -1,29 +1,68 @@
 // @ts-nocheck
 
-// import { parseExpression } from "./boolparser";
+import { Or, Value, Not } from "../ast/ast";
+import { Pair, Nothing } from "../parser/parser";
+import { makeOr, parseVariable, parseExpression } from "./boolparser";
 
 test("makeOr", () => {
-  expect(true).toBe(true);
+  let result = makeOr(new Pair(new Value("a"), new Value("b")));
+  let expected = new Or(new Value("a"), new Value("b"));
+
+  expect(result).toBe(expected);
+
+  result = makeOr(new Pair(new Value("a"), new Nothing()));
+  expected = new Value("a");
+
+  expect(result).toBe(expected);
 });
 
 test("makeAnd", () => {
-  expect(true).toBe(true);
+  let result = makeAnd(new Pair(new Value("a"), new Value("b")));
+  let expect = new And(new Value("a"), new Value("b"));
+
+  expect(result).toBe(expected);
+
+  result = makeAnd(new Pair(new Value("a"), new Nothing()));
+  expected = new Value("a");
+
+  expect(result).toBe(expected);
 });
 
 test("makeNot", () => {
-  expect(true).toBe(true);
+  let result = makeNot(0, new Value("a"));
+  let expected = new Value("a");
+
+  expect(result).toBe(expected);
+
+  result = makeNot(3, new Value("a"));
+  expected = new Not(new Not(new Not(new Value("a"))));
+
+  expect(result).toBe(expected);
 });
 
 test("parseVariable", () => {
-  expect(true).toBe(true);
+  const text = "xyz";
+  const expected = new Value("xyz");
+  const result = parseVariable(stringToInput(text));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
 });
 
 test("parseExclamationMarks", () => {
-  expect(true).toBe(true);
+  const text = "!!!x";
+  const expected = 3;
+  const result = parseExclamationMarks(stringToInput(text));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
 });
 
-test("expression", () => {
-  expect(true).toBe(true);
+test("expression", (text, expected) => {
+  const result = parseExpression(stringToInput(text));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
 });
 
 test("parseExpression", () => {
