710b4edba5e821e27d3c19c0a6eec255ea75e4e7
Markus Voit <m-voit@outlook.com>
2019-12-20 18:59:26 +0100

    Update boolparser.

--- study-project/parser-js/modules/boolparser/boolparser.js
+++ study-project/parser-js/modules/boolparser/boolparser.js
@@ -49,7 +49,7 @@ const parseExpression = () =>
 const parseOr = () =>
   convert(
     andThen(parseAnd(), optional(second(andThen(expect("|"), parseOr())))),
-    makeOr(),
+    makeOr,
   );
 
 /**
@@ -64,7 +64,7 @@ const parseOr = () =>
 const parseAnd = () =>
   convert(
     andThen(parseNot(), optional(second(andThen(expect("&"), parseAnd())))),
-    makeAnd(),
+    makeAnd,
   );
 
 /**
