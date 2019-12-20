ea420fb695f8de1a2761308d2126a11c1476dd85
Markus Voit <m-voit@outlook.com>
2019-12-20 14:12:39 +0100

    Implement boolparser test.

--- study-project/parser-js/modules/boolparser/boolparser.test.js
+++ study-project/parser-js/modules/boolparser/boolparser.test.js
@@ -1,8 +1,13 @@
 // @ts-nocheck
 
-import { Or, Value, Not } from "../ast/ast";
-import { Pair, Nothing } from "../parser/parser";
-import { makeOr, parseVariable, parseExpression } from "./boolparser";
+import { Or, Value, Not, And } from "../ast/ast";
+import { Pair, Nothing, stringToInput } from "../parser/parser";
+import {
+  makeOr,
+  parseVariable,
+  parseExpression,
+  parseExclamationMarks,
+} from "./boolparser";
 
 test("makeOr", () => {
   let result = makeOr(new Pair(new Value("a"), new Value("b")));
@@ -58,13 +63,81 @@ test("parseExclamationMarks", () => {
   expect(result.remainingInput).not.toBe(null);
 });
 
-test("expression", (text, expected) => {
-  const result = parseExpression(stringToInput(text));
+test("parseExpression", () => {
+  let parse = text => parseExpression(stringToInput(text));
+
+  let result = parse("!a");
+  let expected = new Not(new Value("a"));
 
   expect(result.result).toBe(expected);
   expect(result.remainingInput).not.toBe(null);
-});
 
-test("parseExpression", () => {
-  expect(true).toBe(true);
+  result = parse("a&b");
+  expected = new And(new Value("a"), new Value("b"));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a|b");
+  expected = new Or(new Value("a"), new Value("b"));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse(" a &  b");
+  expected = new And(new Value("a"), new Value("b"));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a   ");
+  expected = new Value("a");
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a&b&c");
+  expected = new And(new Value("a"), new And(new Value("b"), new Value("c")));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a&(b&c)");
+  expected = new And(new Value("a"), new And(new Value("b"), new Value("c")));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("(a&b)&c");
+  expected = new And(new And(new Value("a"), new Value("b")), new Value("c"));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a|b|c");
+  expected = new Or(new Value("a"), new Or(new Value("b"), new Value("c")));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("a|(b|c)");
+  expected = new Or(new Value("a"), new Or(new Value("b"), new Value("c")));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("(a|b)|c");
+  expected = new Or(new Or(new Value("a"), new Value("b")), new Value("c"));
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
+
+  result = parse("!a & b|c&!(d|e)");
+  expected = new Or(
+    new And(new Not(new Value("a")), new Value("b")),
+    new And(new Value("c"), new Not(new Or(new Value("d"), new Value("e")))),
+  );
+
+  expect(result.result).toBe(expected);
+  expect(result.remainingInput).not.toBe(null);
 });
