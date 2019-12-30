0f0ded0ae8d6888727bb56757f7db816da527612
Markus Voit <m-voit@outlook.com>
2019-12-30 21:55:55 +0100

    Add parser tests.

--- study-project/parser-js/modules/parser/parser.test.js
+++ study-project/parser-js/modules/parser/parser.test.js
@@ -1,5 +1,58 @@
 // @ts-nocheck
 
-test("parser", () => {
-  expect(true).toBe(true);
+import {
+  isIdentifierStartChar,
+  isDigit,
+  isIdentifierChar,
+  isSpaceChar,
+} from "./parser";
+
+describe("Test parser.js", () => {
+  test("isIdentifierStartChar", () => {
+    let result = isIdentifierStartChar("a");
+    let expected = true;
+
+    expect(result).toStrictEqual(expected);
+
+    result = isIdentifierStartChar("ä");
+    expected = false;
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("isDigit", () => {
+    let result = isDigit("0");
+    let expected = true;
+
+    expect(result).toStrictEqual(expected);
+
+    result = isIdentifierStartChar("ä");
+    expected = false;
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("isIdentifierChar", () => {
+    let result = isIdentifierChar("a");
+    let expected = true;
+
+    expect(result).toStrictEqual(expected);
+
+    result = isIdentifierStartChar("ä");
+    expected = false;
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("isSpaceChar", () => {
+    let result = isSpaceChar(" ");
+    let expected = true;
+
+    expect(result).toStrictEqual(expected);
+
+    result = isIdentifierStartChar("ä");
+    expected = false;
+
+    expect(result).toStrictEqual(expected);
+  });
 });
