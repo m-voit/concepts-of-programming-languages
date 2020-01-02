41f1ca45f932f611ec76374163813a202832c47b
Markus Voit <m-voit@outlook.com>
2020-01-02 15:52:36 +0100

    Update boolparser.

--- study-project/parser-js/modules/boolparser/boolparser.js
+++ study-project/parser-js/modules/boolparser/boolparser.js
@@ -32,24 +32,24 @@ import {
  */
 
 /**
- * Parse the following grammar: Expression := Or Spaces*
- * The syntax tree is exactly the one returned by Or.
+ * Parse the grammar: Expression := Or Spaces*
+ * The syntax tree is exactly the one returned by OR.
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns An AST of the parsed expression.
  */
 export const parseExpression = input =>
   first(andThen(parseOr, optional(expectSpaces)))(input);
 
 /**
- * Parse the following grammar: Or := And ^ ("|" ^ Or)?
+ * Parse the grammar: Or := And ^ ("|" ^ Or)?
  * If the parser for ("|" ^ Or)? produces nothing then return the
- * tree returned by And. Otherwise return a new Or Node containing
+ * tree returned by And. Otherwise return a new OR Node containing
  * the sub-trees returned by the recursive calls. It uses expect to parse
  * the symbol "|", i. e. it actually allows for Space* ^ "|".
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A tree of AST nodes.
  */
 export const parseOr = input =>
   convert(
@@ -58,14 +58,14 @@ export const parseOr = input =>
   )(input);
 
 /**
- * Parse the following grammar: And := Not ^ ("&" ^ And)?
+ * Parse the grammar: And := Not ^ ("&" ^ And)?
  * If the parser for ("&" ^ And)? produces nothing then return the
- * tree returned by Not. Otherwise return a new And Node containing
+ * tree returned by Not. Otherwise return a new AND Node containing
  * the sub-trees returned by the recursive calls. It uses expect to parse
  * the symbol "&", i. e. it actually allows for Space* ^ "&".
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A tree of AST nodes.
  */
 export const parseAnd = input =>
   convert(
@@ -74,14 +74,14 @@ export const parseAnd = input =>
   )(input);
 
 /**
- * Parse the following grammar: Not := "!"* ^ Atom
- * It delegates parsing "!"* to parseExclamationMarks and the construction of Not
- * nodes to makeNots. If there's no exclamation mark then parseNot will return
- * the tree parsed by parseAtom. Otherwise parseNot will wrap the atom in as many
- * Not nodes as there are exclamation marks.
+ * Parse the grammar: Not := "!"* ^ Atom
+ * It delegates parsing "!"* to parseExclamationMarks() and the
+ * construction of Not nodes to makeNot(). If there's no exclamation
+ * mark then return the tree parsed by parseAtom(). Otherwise wrap
+ * the atom in as many NOT nodes as there are exclamation marks.
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A tree of AST nodes.
  */
 export const parseNot = input =>
   convert(andThen(parseExclamationMarks, parseAtom), pair => {
@@ -89,25 +89,24 @@ export const parseNot = input =>
   })(input);
 
 /**
- * Parse the following grammar: "!"*
- * Return the number of exclamation marks in Result. Result as an int.
+ * Parse the grammar: "!"*
+ * Return the number of exclamation marks in result as a number.
  * It uses expect to parse the symbol "!", i. e. it actually
  * allows for Space* ^ "!".
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A new result.
  */
 export const parseExclamationMarks = input =>
   convert(repeated(expect("!")), arg => arg.length)(input);
 
 /**
- * Parse the followiong grammar: Atom := Variable | "(" ^ Expression ^ ")"
+ * Parse the grammar: Atom := Variable | "(" ^ Expression ^ ")"
  * The parenthesis won't appear in the abstract syntax tree. It uses
- * Parser.First() and Parser.Second() to extract the tree returned by
- * parseExpression.
+ * first() and second() to extract the tree returned by parseExpression().
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A tree of AST nodes.
  */
 export const parseAtom = input =>
   orElse(
@@ -116,19 +115,18 @@ export const parseAtom = input =>
   )(input);
 
 /**
- * Parse the following grammar: Variable := [a-zA-Z_][a-zA-Z_0-9]*
- * It delegates parsing the variable name to ExpectIdentifier from the parser
- * combinators package and uses the Convert method on parsers to create the
- * ast.Val node.
+ * Parse the grammar: Variable := [a-zA-Z_][a-zA-Z_0-9]*
+ * It delegates parsing the variable name to the expectIdentifier() function
+ * and uses the convert() function to create the VALUE node.
  *
  * @param {Input} input The input to be parsed.
- * @returns A result.
+ * @returns A new VALUE node.
  */
 export const parseVariable = input =>
   convert(maybeSpacesBefore(expectIdentifier), arg => new Value(arg))(input);
 
 /**
- * Wrap the node into Not nodes.
+ * Wrap nodes into NOT nodes.
  *
  * @param {number} number The number of NOT nodes to wrap the node in.
  * @param {any} node The AST node to be wrapped into NOT nodes.
@@ -141,7 +139,7 @@ export const makeNot = (number, node) =>
  * Take a Pair of Nodes as an argument and return a Node.
  * If the second component of the pair is equal to Nothing then return
  * the first component of the Pair. If the second component is a Node
- * then create an And node containing the first and the second
+ * then create an AND node containing the first and the second
  * component of the Pair as sub-nodes.
  *
  * @param {Pair} pair A pair of Nodes.
@@ -154,9 +152,9 @@ export const makeAnd = pair =>
 
 /**
  * Take a Pair of Nodes as an argument and return a Node.
- * If the second component of the pair is equal to Nothing{} then return
- * the first component of the Pair. If the second component is a Node
- * then create an Or node containing the first and the second
+ * If the second component of the pair is an instanceof Nothing() then
+ * return the first component of the Pair. If the second component is
+ * a Node then create an OR node containing the first and the second
  * component of the Pair as sub-nodes.
  *
  * @param {Pair} pair A pair of Nodes.
